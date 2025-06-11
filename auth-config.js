92f2ef2bc2d8a0ad53fc2b2da902285408136952
luciano iury castro pedrosa <lucianoiury11@gmail.com>
2025-06-11 20:45:45 -0300

    Adiciona funcionalidade de cadastro com Supabase

---
+++ auth-config.js
@@ -0,0 +1,8 @@
+// auth-config.js
+
+// Cole aqui a URL e a Chave de API (anon public) do seu projeto Supabase
+// Você encontra isso em: Project Settings > API
+const SUPABASE_URL = 'https://zteetrqjvkgzkquyhocj.supabase.co';
+const SUPABASE_KEY = 'SeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZWV0cnFqdmtnemtxdXlob2NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NzcxMTEsImV4cCI6MjA2NTI1MzExMX0.Qn--GbpQQMphZo5s4V3spodRAqvjc2xFVrwhfRkrY6M';
+
+// OBS: Certifique-se de que este arquivo seja carregado ANTES do seu script principal (auth.js) no HTML.
